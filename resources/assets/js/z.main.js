720432c0106c6ca9ca802b21b0d81ed335d9ee23
sameerrM <samir_nox@hotmail.com>
2019-05-15 00:16:13 +0200

    counter number section

---
+++ resources/assets/js/z.main.js
@@ -0,0 +1,20 @@
+// Counter number 
+jQuery(window).scroll(startCounter);
+function startCounter() {
+    var hT = jQuery('.counter-number-text').offset().top,
+        hH = jQuery('.counter-number-text').outerHeight(),
+        wH = jQuery(window).height();
+    if (jQuery(window).scrollTop() > hT+hH-wH) {
+        jQuery(window).off("scroll", startCounter);
+        jQuery('.counter').each(function () {
+            var $this = jQuery(this);
+            jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                duration: 2000,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter));
+                }
+            });
+        });
+    }
+}
