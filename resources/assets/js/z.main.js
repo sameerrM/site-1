881c482520e32fdf57bf48dfd5a7e94d2619167d
sameerrM <samir_nox@hotmail.com>
2019-05-18 01:27:43 +0200

    some section added

--- resources/assets/js/z.main.js
+++ resources/assets/js/z.main.js
@@ -1,24 +1,32 @@
-// Counter number 
-jQuery(window).scroll(startCounter);
-function startCounter() {
-    var hT = jQuery('.counter-number-text').offset().top,
-        hH = jQuery('.counter-number-text').outerHeight(),
-        wH = jQuery(window).height();
-    if (jQuery(window).scrollTop() > hT+hH-wH) {
-        jQuery(window).off("scroll", startCounter);
-        jQuery('.counter').each(function () {
-            var $this = jQuery(this);
-            jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-                duration: 2000,
-                easing: 'swing',
-                step: function () {
-                    $this.text(Math.ceil(this.Counter));
-                }
-            });
-        });
-    }
-}
 
-$('.navbar-toggler').on('click', function(){
-    $(this).toggleClass('menu-open');
+$(function() {
+    
+    // Counter number 
+    jQuery(window).scroll(startCounter);
+    function startCounter() {
+        var hT = jQuery('.counter-number-text').offset().top,
+            hH = jQuery('.counter-number-text').outerHeight(),
+            wH = jQuery(window).height();
+        if (jQuery(window).scrollTop() > hT+hH-wH) {
+            jQuery(window).off("scroll", startCounter);
+            jQuery('.counter').each(function () {
+                var $this = jQuery(this);
+                jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+        }
+    }
+
+    $('.navbar-toggler').on('click', function(){
+        $(this).toggleClass('menu-open');
+    });
+
+    $('.parallax-window').parallax({imageSrc: 'assets/img/bg-parallax.jpg'});
+
+	$('.blog-info p').matchHeight();
 });
